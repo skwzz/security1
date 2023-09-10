c91f7faaa0e3f31171fb98d811b606cf69c8a52e
skwzz <kidol9108@gmail.com>
2023-09-10 22:52:04 +0900

    Google Oauth2.0 로그인 준비

--- src/main/java/com/skwzz/security1/config/SecurityConfig.java
+++ src/main/java/com/skwzz/security1/config/SecurityConfig.java
@@ -26,10 +26,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
             .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
             .anyRequest().permitAll()
-        .and()
+            .and()
             .formLogin()
             .loginPage("/loginForm")
             .loginProcessingUrl("/login") // login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행해줌
-            .defaultSuccessUrl("/");
+            .defaultSuccessUrl("/")
+            .and()
+            .oauth2Login()
+            .loginPage("/loginForm"); // 구글 로그인이 완료된 뒤의 후처리가 필요함
     }
 }
